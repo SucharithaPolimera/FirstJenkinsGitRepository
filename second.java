5c29b05bcfa250180ed542a5f4742ff38d2ce371
SucharithaPolimera <91176799+SucharithaPolimera@users.noreply.github.com>
2021-11-26 14:42:51 +0530

    Update second.java

--- second.java
+++ second.java
@@ -2,7 +2,7 @@ public class Suchi
 {
 public static void main(String args[])
 {
-System.out.print("Hello,I am Sucharitha");
-  System.out.print("Hi,I am Jenkins");
+System.out.println("Hello,I am Sucharitha");
+System.out.println("Hi,I am Jenkins");
 }
 }
